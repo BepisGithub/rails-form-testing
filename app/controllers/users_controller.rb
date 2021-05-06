29d646a2d4da6b798482fcaca6137d8a38f83fff
bepis <luqdek@gmail.com>
2021-05-06 20:40:47 +0100

    add code for update controller method and edit method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+
   def create
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
   end
 
