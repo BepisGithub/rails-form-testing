106ac67eba9d46fc6f96807fc5a21923481e109f
bepis <luqdek@gmail.com>
2021-05-06 22:59:58 +0100

    add flashes to indicate failure and fix user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,21 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    if User.exists? params[:id]
+      @user = User.find(params[:id])
+    else
+      flash[:danger] = 'User doesn\'t exist'
+      redirect_to new_user_path
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      flash[:success] = 'user saved'
+      redirect_to @user
     else
+      flash[:danger] = 'User cant be saved'
       render :new
     end
   end
@@ -22,15 +29,18 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update_attributes(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'user saved'
       redirect_to @user
     else
+      flash[:danger] = 'User cant be saved'
       render :edit
     end
   end
 
   private
     def user_params
-      params.permit(:username,:password,:email)
+      params.require(:user).permit(:username,:password,:email)
     end
 end
